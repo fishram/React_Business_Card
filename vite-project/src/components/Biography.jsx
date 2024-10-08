3c014af8dd1e0e61b8949b39394442527897ed24
Fisher Ramsey <fishram@unc.edu>
2024-10-08 15:17:45 -0400

    Finished styling business card Added email button functionality

--- vite-project/src/components/Biography.jsx
+++ vite-project/src/components/Biography.jsx
@@ -2,12 +2,33 @@ import React from "react"
 
 export default function Biography() { 
     return (
-        <div className="biography-container">
-            <h2>About</h2>
-            <h2>Skills</h2>
-            <ul className="skills-list">
-
-            </ul>
+        <div className="biography">
+            <h2>About Me</h2>
+            <p>I'm a Junior at UNC Chapel Hill studying both computer science and economics.
+               In my free time I like to snowboard and travel.
+            </p>
+            <div className="skills-container">
+                <div className="languages">
+                    <h3>Languages</h3>
+                    <ul className="skills-list">
+                        <li>Java</li>
+                        <li>Python</li>
+                        <li>JavaScript</li>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                    </ul>
+                </div>
+                <div className="Technologies">
+                    <h3>Technologies</h3>
+                        <ul className="skills-list">
+                            <li>React</li>
+                            <li>RESTful API</li>
+                            <li>Git</li>
+                            <li>Flask</li>
+                            <li>JSON</li>
+                        </ul>
+                </div>
+            </div>
         </div>
     )
 }
--- vite-project/src/components/PersonalInfo.jsx
+++ vite-project/src/components/PersonalInfo.jsx
@@ -2,11 +2,14 @@ import React from "react"
 import email_icon from "../assets/email_icon.png"
 
 export default function PersonalInfo() {
+    const handleEmailClick = () => {
+        window.location.href = 'mailto:fishram@unc.edu?subject=Hello%20Fisher&body=I%20would%20like%20to%20get%20in%20touch.';
+    };
     return (
         <div className="personal-info-box">
             <h1>Fisher Ramsey</h1>
             <h3>Computer Science @ UNC</h3>
-            <button><img src={email_icon} className="email-icon"></img>Email</button>
+            <button onClick={handleEmailClick}><img src={email_icon} className="email-icon"></img>Email</button>
         </div>
     )
 }
